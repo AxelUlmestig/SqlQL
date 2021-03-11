6db172e505923d0335ce276cdb4bf8a4e1a0f4a1
Axel Ulmestig <axel.ulmestig@gmail.com>
2021-03-11 19:30:59 +0100

    add first draft of parser

---
+++ src/Parser.hs
@@ -0,0 +1,177 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Parser (sqlP) where
+
+import           Prelude                            hiding (tail)
+
+import qualified Control.Monad.Combinators.NonEmpty as NE
+import           Data.Functor                       (($>))
+import           Data.List.NonEmpty                 (NonEmpty)
+import           Data.Text                          (Text, breakOn, pack, tail)
+import           Data.Tuple                         (swap)
+import           Data.Void                          (Void)
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer         as L
+
+import           Sql
+
+type Parser = Parsec Void Text
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme space
+
+sqlP :: Parser SQL
+sqlP = lexeme $ space
+                $>  Select
+                <*  lexeme (string' "select")
+                <*> selectColumnsP
+                <*> optional fromP
+                <*> optional whereP
+                <*> optional groupByP
+                <*> optional orderByP
+                <*> optional limitP
+
+-- select
+selectColumnsP :: Parser (NonEmpty SelectColumn)
+selectColumnsP = lexeme $ NE.sepBy1 selectColumnP (lexeme (char ','))
+
+selectColumnP :: Parser SelectColumn
+selectColumnP = lexeme $
+                  starP
+                  <|> try tableStarP
+                  <|> (SelectColumn <$> alias expressionP)
+  where
+    starP       = SelectAll <$ char '*'
+    tableStarP  = SelectAllTable <$> tableNameP <* string ".*"
+
+columnP :: Parser Column
+columnP = do
+  x <- columnNameP
+  y <- optional (char '.' *> columnNameP)
+  return $ maybe (Column x) (TableColumn x) y
+
+expressionP :: Parser Expression
+expressionP = do
+                maybeParens <- optional (char '(')
+                case maybeParens of
+                  Nothing -> do
+                    exp <- singleExpP
+                    combinedExpP exp
+                  (Just _) -> do
+                    exp <- expressionP <* char ')'
+                    combinedExpP (ParensedExp exp)
+  where
+    combinedExpP exp1 = do
+                          maybeOp <- optional expOpP
+                          case maybeOp of
+                            Nothing -> return exp1
+                            (Just op) -> do
+                              exp2 <- expressionP
+                              combinedExpP (MetaExpression op exp1 exp2)
+
+    singleExpP = lexeme $
+                   ExpTrue <$ string' "true"
+                   <|> ExpFalse <$ string' "false"
+                   <|> ExpString . pack <$ char '\'' <*> manyTill anySingle (char '\'')
+                   <|> ExpColumn <$> columnP
+                   <|> ExpInt <$> L.signed space L.decimal
+                   <|> ExpFloat <$> L.signed space L.float
+
+    expOpP :: Parser ExpressionOperator
+    expOpP = lexeme $
+               ExpAnd                   <$ string' "and" <* space1
+               <|> try (ExpOr           <$ string' "or"  <* space1)
+               <|> ExpAdd               <$ char    '+'
+               <|> ExpSubtract          <$ char    '-'
+               <|> ExpMultiply          <$ char    '*'
+               <|> ExpDivide            <$ char    '/'
+               <|> ExpGreaterThan       <$ char    '>'
+               <|> ExpGreaterThanEqual  <$ string' ">="
+               <|> ExpLesserThan        <$ char    '<'
+               <|> ExpLesserThanEqual   <$ string' "<="
+               <|> ExpExponentiate      <$ char    '^'
+
+columnNameP :: Parser Text
+columnNameP = fmap pack $
+              (:)
+              <$> (letterChar <|> char '_')
+              <*> many (alphaNumChar <|> char '_')
+
+-- from
+fromP :: Parser From
+fromP = From
+        <$  lexeme (string' "from" <* space1)
+        <*> alias tableNameP
+        <*> many joinP
+
+tableNameP :: Parser Text
+tableNameP = columnNameP
+
+-- TODO: handle inner join without inner
+joinP :: Parser Join
+joinP = lexeme (innerJoinP <|> leftJoinP <|> crossJoinP)
+  where
+    innerJoinP = InnerJoin
+                 <$  lexeme (string' "inner join" <* space1)
+                 <*> alias tableNameP
+                 <*  lexeme (string' "on" <* space1)
+                 <*> expressionP
+    leftJoinP =  LeftJoin
+                 <$  lexeme (string' "left join" <* space1)
+                 <*> alias tableNameP
+                 <*  lexeme (string' "on" <* space1)
+                 <*> expressionP
+    crossJoinP = CrossJoin
+                 <$  lexeme (string' "cross join" <* space1)
+                 <*> alias tableNameP
+
+-- where
+whereP :: Parser Where
+whereP = lexeme $
+           Where
+           <$  lexeme (string' "where" <* space1)
+           <*> expressionP
+
+-- group by
+groupByP :: Parser GroupBy
+groupByP = lexeme $
+             string' "group by"
+             <*  space1
+             <*  space
+             $>  GroupBy
+             <*> NE.sepBy1 expressionP (lexeme (char ','))
+
+-- order by
+orderByP :: Parser OrderBy
+orderByP = lexeme $
+             string' "order by"
+             <*  space1
+             $>  OrderBy
+             <*> NE.sepBy1 orderExpressionP (lexeme (char ','))
+
+orderExpressionP :: Parser OrderExpression
+orderExpressionP = OrderExpression
+                   <$> expressionP
+                   <*> optional orderDirectionP
+  where
+    orderDirectionP = (Asc <$ string' "asc")
+                      <|> (Desc <$ string' "desc")
+
+-- limit
+limitP :: Parser Limit
+limitP = lexeme $
+           string' "limit"
+           <* space1
+           <* space
+           *> (
+             (Limit <$> L.decimal)
+             <|> (LimitAll <$ string' "all")
+           )
+
+-- TODO: what about schemas?
+alias :: Parser a -> Parser (Aliased a)
+alias p = lexeme $ do
+  x <- lexeme p
+  a <- optional (lexeme (string' "as ") *> columnNameP)
+  return (Aliased x a)
---
+++ src/Sql.hs
@@ -0,0 +1,96 @@
+module Sql (
+  SQL(..),
+  Column(..),
+  SelectColumn(..),
+  From(..),
+  Join(..),
+  Where(..),
+  GroupBy(..),
+  Expression(..),
+  ExpressionOperator(..),
+  OrderBy(..),
+  OrderExpression(..),
+  OrderDirection(..),
+  Limit(..),
+  Aliased(..)
+) where
+
+import           Data.List.NonEmpty (NonEmpty)
+import           Data.Text          (Text)
+
+type TableName = Text
+type ColumnName = Text
+
+data SQL = Select (NonEmpty SelectColumn) (Maybe From) (Maybe Where) (Maybe GroupBy) (Maybe OrderBy) (Maybe Limit)
+         deriving (Show)
+
+-- select
+data SelectColumn = SelectColumn (Aliased Expression)
+                  | SelectAll
+                  | SelectAllTable TableName
+                  deriving (Show)
+
+data Column = Column ColumnName
+            | TableColumn TableName ColumnName
+            deriving (Show)
+
+data Aliased a = Aliased a (Maybe Text)
+               deriving (Show)
+
+data Expression = ExpTrue
+                | ExpFalse
+                | ExpString Text
+                | ExpInt Int
+                | ExpFloat Float
+                | MetaExpression ExpressionOperator Expression Expression
+                | ExpColumn Column
+                | ParensedExp Expression
+                deriving (Show)
+
+data ExpressionOperator = ExpAnd
+                        | ExpOr
+                        | ExpAdd
+                        | ExpSubtract
+                        | ExpMultiply
+                        | ExpDivide
+                        | ExpGreaterThan
+                        | ExpGreaterThanEqual
+                        | ExpLesserThan
+                        | ExpLesserThanEqual
+                        | ExpExponentiate
+                        deriving (Show)
+
+-- from
+data From = From (Aliased TableName) [Join]
+          deriving (Show)
+
+data Join = InnerJoin (Aliased TableName) Expression
+          | LeftJoin (Aliased TableName) Expression
+          | CrossJoin (Aliased TableName)
+          deriving (Show)
+
+-- where
+data Where = Where Expression
+           deriving (Show)
+
+-- group by
+-- TODO: 'group by ()' is technically valid SQL
+data GroupBy = GroupBy (NonEmpty Expression)
+             deriving (Show)
+
+-- order by
+data OrderBy = OrderBy (NonEmpty OrderExpression)
+             deriving (Show)
+
+data OrderExpression = OrderExpression Expression (Maybe OrderDirection)
+                     deriving (Show)
+
+data OrderDirection = Asc | Desc
+                    deriving (Show)
+
+-- limit
+data Limit = Limit Int | LimitAll
+           deriving (Show)
+
+-- TODO: group by
+

66522498dbd14f8951948412b4a13091941d1710
AxelTLarsson <mail@axellarsson.nu>
2021-03-12 17:23:00 +0100

    🎨 Format Format.hs with stylish-haskell

--- src/SQL/Format.hs
+++ src/SQL/Format.hs
@@ -1,19 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module SQL.Format
-  ( format
-  ) where
+module SQL.Format (
+  format
+) where
 
-import Data.List.NonEmpty (NonEmpty)
-import Data.Text (Text, pack)
+import           Data.List.NonEmpty (NonEmpty)
+import           Data.Text          (Text, pack)
 
-import SQL.AST
+import           SQL.AST
 
 format :: SQL -> Text
 format (Select cols from wher groupBy maybeOrderBy maybeLimit) =
-  "select " <>
-  formatSelectColumns cols <>
-  maybe "" formatLimit maybeLimit <> maybe "" formatOrderBy maybeOrderBy
+  "select "
+  <> formatSelectColumns cols
+  <> maybe "" formatLimit maybeLimit
+  <> maybe "" formatOrderBy maybeOrderBy
 
 formatSelectColumns :: NonEmpty SelectColumn -> Text
 formatSelectColumns = foldr (<>) "" . fmap ((<> ", ") . formatSelectColumn)
@@ -21,8 +22,7 @@ formatSelectColumns = foldr (<>) "" . fmap ((<> ", ") . formatSelectColumn)
 formatSelectColumn :: SelectColumn -> Text
 formatSelectColumn SelectAll = "*"
 formatSelectColumn (SelectAllTable name) = name <> ".*"
-formatSelectColumn (SelectColumn (Aliased exp mAlias)) =
-  maybe formattedExp (formattedExp <>) ((" as " <>) <$> mAlias)
+formatSelectColumn (SelectColumn (Aliased exp mAlias)) = maybe formattedExp (formattedExp <>) ((" as " <>) <$> mAlias)
   where
     formattedExp = formatExpression exp
 
@@ -30,23 +30,23 @@ formatExpression :: Expression -> Text
 formatExpression (ExpColumn col) = formatColumn col
 
 formatColumn :: Column -> Text
-formatColumn (Column col) = col
+formatColumn (Column col)          = col
 formatColumn (TableColumn tbl col) = tbl <> "." <> col
 
 formatLimit :: Limit -> Text
-formatLimit (LimitAll) = "limit all"
+formatLimit (LimitAll)     = "limit all"
 formatLimit (Limit number) = "limit " <> pack (show number)
 
 formatOrderBy :: OrderBy -> Text
 formatOrderBy (OrderBy (orders)) =
-  "order by " <>
-  foldr (<>) "" (fmap ((<> ", ") . formatOrderExpression) (orders))
+  "order by "
+  <> foldr (<>) "" (fmap ((<> ", ") . formatOrderExpression) (orders))
 
 formatOrderExpression :: OrderExpression -> Text
 formatOrderExpression (OrderExpression (exp) maybeDir) =
-  formatExpression exp <>
-  maybe "" (fmap (\x -> " " <> x) formatOrderDirection) maybeDir
+  formatExpression exp
+  <> maybe "" (fmap (\x -> " " <> x) formatOrderDirection) maybeDir
 
 formatOrderDirection :: OrderDirection -> Text
-formatOrderDirection (Asc) = "asc"
+formatOrderDirection (Asc)  = "asc"
 formatOrderDirection (Desc) = "desc"

085e0456eb5df7c3eb137ab17991d621eec91a3f
Johannes Jansson <johannes.e.jansson@gmail.com>
2021-03-12 15:27:43 +0100

    Add limit to formatter

--- src/SQL/Format.hs
+++ src/SQL/Format.hs
@@ -5,14 +5,15 @@ module SQL.Format (
 ) where
 
 import           Data.List.NonEmpty (NonEmpty)
-import           Data.Text          (Text)
+import           Data.Text          (Text, pack)
 
 import           SQL.AST
 
 format :: SQL -> Text
-format (Select cols from wher groupBy orderBy limit) =
+format (Select cols from wher groupBy orderBy maybeLimit) =
   "select "
   <> formatSelectColumns cols
+  <> maybe "" formatLimit maybeLimit
 
 formatSelectColumns :: NonEmpty SelectColumn -> Text
 formatSelectColumns = foldr (<>) "" . fmap ((<> ", ") . formatSelectColumn)
@@ -20,3 +21,7 @@ formatSelectColumns = foldr (<>) "" . fmap ((<> ", ") . formatSelectColumn)
 formatSelectColumn :: SelectColumn -> Text
 formatSelectColumn SelectAll             = "*"
 formatSelectColumn (SelectAllTable name) = name <> ".*"
+
+formatLimit :: Limit -> Text
+formatLimit (LimitAll) = "limit all"
+formatLimit (Limit number) = "limit " <> pack (show number)

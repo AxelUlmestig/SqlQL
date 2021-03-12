975d83efab28c35cd16a9e0130a75ba9aae2247f
AxelTLarsson <mail@axellarsson.nu>
2021-03-12 17:13:17 +0100

    ✨ Suport orderBy expressions in formatteR
    Ironically (I think?) my editor setup re-formatted the source code of
    Fomat.hs when I saved it - apparently I use hindent for that, we should
    probably explicitly decide if we should go ahead and use that or not.

--- src/SQL/Format.hs
+++ src/SQL/Format.hs
@@ -1,27 +1,28 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module SQL.Format (
-  format
-) where
+module SQL.Format
+  ( format
+  ) where
 
-import           Data.List.NonEmpty (NonEmpty)
-import           Data.Text          (Text, pack)
+import Data.List.NonEmpty (NonEmpty)
+import Data.Text (Text, pack)
 
-import           SQL.AST
+import SQL.AST
 
 format :: SQL -> Text
-format (Select cols from wher groupBy orderBy maybeLimit) =
-  "select "
-  <> formatSelectColumns cols
-  <> maybe "" formatLimit maybeLimit
+format (Select cols from wher groupBy maybeOrderBy maybeLimit) =
+  "select " <>
+  formatSelectColumns cols <>
+  maybe "" formatLimit maybeLimit <> maybe "" formatOrderBy maybeOrderBy
 
 formatSelectColumns :: NonEmpty SelectColumn -> Text
 formatSelectColumns = foldr (<>) "" . fmap ((<> ", ") . formatSelectColumn)
 
 formatSelectColumn :: SelectColumn -> Text
-formatSelectColumn SelectAll             = "*"
+formatSelectColumn SelectAll = "*"
 formatSelectColumn (SelectAllTable name) = name <> ".*"
-formatSelectColumn (SelectColumn (Aliased exp mAlias)) = maybe formattedExp (formattedExp <>) ((" as " <>) <$> mAlias)
+formatSelectColumn (SelectColumn (Aliased exp mAlias)) =
+  maybe formattedExp (formattedExp <>) ((" as " <>) <$> mAlias)
   where
     formattedExp = formatExpression exp
 
@@ -29,9 +30,23 @@ formatExpression :: Expression -> Text
 formatExpression (ExpColumn col) = formatColumn col
 
 formatColumn :: Column -> Text
-formatColumn (Column col)          = col
+formatColumn (Column col) = col
 formatColumn (TableColumn tbl col) = tbl <> "." <> col
 
 formatLimit :: Limit -> Text
-formatLimit (LimitAll)     = "limit all"
+formatLimit (LimitAll) = "limit all"
 formatLimit (Limit number) = "limit " <> pack (show number)
+
+formatOrderBy :: OrderBy -> Text
+formatOrderBy (OrderBy (orders)) =
+  "order by " <>
+  foldr (<>) "" (fmap ((<> ", ") . formatOrderExpression) (orders))
+
+formatOrderExpression :: OrderExpression -> Text
+formatOrderExpression (OrderExpression (exp) maybeDir) =
+  formatExpression exp <>
+  maybe "" (fmap (\x -> " " <> x) formatOrderDirection) maybeDir
+
+formatOrderDirection :: OrderDirection -> Text
+formatOrderDirection (Asc) = "asc"
+formatOrderDirection (Desc) = "desc"
